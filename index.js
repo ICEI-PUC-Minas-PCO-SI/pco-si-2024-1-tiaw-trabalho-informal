317146ea188b62881b839bf8f392c640833bed75
Rommel Vieira Carneiro <rommelcarneiro@gmail.com>
2024-04-29 13:03:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Trabalho Interdisciplinar 1 - Aplicações Web
+//
+// Esse módulo implementa uma API RESTful baseada no JSONServer
+// O servidor JSONServer fica hospedado na seguinte URL
+// https://jsonserver.rommelpuc.repl.co/contatos
+//
+// Para montar um servidor para o seu projeto, acesse o projeto 
+// do JSONServer no Replit, faça o FORK do projeto e altere o 
+// arquivo db.json para incluir os dados do seu projeto.
+//
+// URL Projeto JSONServer: https://replit.com/@rommelpuc/JSONServer
+//
+// Autor: Rommel Vieira Carneiro
+// Data: 03/10/2023
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./db/db.json')
+const cors = require('cors');
+
+// Para permitir que os dados sejam alterados, altere a linha abaixo
+// colocando o atributo readOnly como false.
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
---
+++ public/app_contatos/scripts/app.js
@@ -0,0 +1,88 @@
+// Trabalho Interdisciplinar 1 - Aplicações Web
+//
+// Esse módulo realiza as operações de CRUD a partir de uma API baseada no JSONServer
+// O servidor JSONServer fica hospedado na seguinte URL
+// https://jsonserver.rommelpuc.repl.co/contatos
+//
+// Para fazer o seu servidor, acesse o projeto do JSONServer no Replit, faça o 
+// fork do projeto e altere o arquivo db.json para incluir os dados do seu projeto.
+// URL Projeto JSONServer: https://replit.com/@rommelpuc/JSONServer
+//
+// Autor: Rommel Vieira Carneiro
+// Data: 03/10/2023
+
+// URL da API JSONServer - Substitua pela URL correta da sua API
+const apiUrl = '/contatos';
+
+function displayMessage(mensagem) {
+    msg = document.getElementById('msg');
+    msg.innerHTML = '<div class="alert alert-warning">' + mensagem + '</div>';
+}
+
+function readContato(processaDados) {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            processaDados(data);
+        })
+        .catch(error => {
+            console.error('Erro ao ler contatos via API JSONServer:', error);
+            displayMessage("Erro ao ler contatos");
+        });
+}
+
+function createContato(contato, refreshFunction) {
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(contato),
+    })
+        .then(response => response.json())
+        .then(data => {
+            displayMessage("Contato inserido com sucesso");
+            if (refreshFunction)
+                refreshFunction();
+        })
+        .catch(error => {
+            console.error('Erro ao inserir contato via API JSONServer:', error);
+            displayMessage("Erro ao inserir contato");
+        });
+}
+
+function updateContato(id, contato, refreshFunction) {
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(contato),
+    })
+        .then(response => response.json())
+        .then(data => {
+            displayMessage("Contato alterado com sucesso");
+            if (refreshFunction)
+                refreshFunction();
+        })
+        .catch(error => {
+            console.error('Erro ao atualizar contato via API JSONServer:', error);
+            displayMessage("Erro ao atualizar contato");
+        });
+}
+
+function deleteContato(id, refreshFunction) {
+    fetch(`${apiUrl}/${id}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
+        .then(data => {
+            displayMessage("Contato removido com sucesso");
+            if (refreshFunction)
+                refreshFunction();
+        })
+        .catch(error => {
+            console.error('Erro ao remover contato via API JSONServer:', error);
+            displayMessage("Erro ao remover contato");
+        });
+}
