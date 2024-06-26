70916c1e8bc5dc4670a0a8c75299e1d213c21a29
Yalle Ramos <107776066+ieiou@users.noreply.github.com>
2024-06-26 08:21:14 -0300

    Update chat.js
    Alterei o caminho da pasta Json pois estava dando erro , ontem quando fui testar com a amália não estava recuperando do json por isso , agora  está arrumado , tudo bem que é apenas provisório pois irá buscar do Json Server mas tá aí galerinha.

--- Chat - Ajustado-Yalle/js/chat.js
+++ Chat - Ajustado-Yalle/js/chat.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('json/users.json')
+    fetch('js/json/users.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
