ebb8487707676b3a20adabb9018105be4170f236
Yalle Ramos <107776066+ieiou@users.noreply.github.com>
2024-06-26 08:26:56 -0300

    update chat.js
    Ignorem o commit anterior

--- Chat - Ajustado-Yalle/js/chat.js
+++ Chat - Ajustado-Yalle/js/chat.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('js/json/users.json')
+    fetch('json/users.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
